54a83d074413f4b0f5c967b66f48e9643a9ef296
Ravi <raviwebdevelopment1762@gmail.com>
2026-07-11 20:46:54 +0530

    .

--- assets/builder/build/index.asset.php
+++ assets/builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b944828dfddc4209de5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '34968d81b121b5205dce');
